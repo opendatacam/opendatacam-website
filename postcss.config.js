186ed5590c1b5ffb098bd8ed554051cb21edcfea
Thibault Durand <tibo.durand@gmail.com>
2019-06-27 12:35:39 +0200

    Evo: configure purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,22 @@
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  // For now do not do anything
+  content: ['./components/*.js','./pages/*.js'],
+  whitelist: ['html', 'body'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
   plugins: [
     require('postcss-easy-import'),
     require('tailwindcss'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
