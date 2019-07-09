4166d7651f063ee9a14d14800ae6b6467c02cb1c
Thibault Durand <tibo.durand@gmail.com>
2019-07-09 17:39:11 +0200

    WIP: font text

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,17 @@ import Layout from '../components/Layout';
 export default () => (
   <Layout>
     <Nav />
-    <div className='hero'>
-      <h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-medium text-center'>
+    <div className='hero flex justify-center'>
+      <h1 className='
+        text-2xl 
+        sm:text-3xl 
+        md:text-4xl 
+        lg:text-5xl 
+        font-medium
+        text-center
+        max-w-lg
+        pl-8
+        pr-8'>
         An Open Source Camera to Collect Your Spatial Data
       </h1>
     </div>
