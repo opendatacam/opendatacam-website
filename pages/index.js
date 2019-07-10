539121c24a7ca403afeed9577eb36c45541d97b5
Thibault Durand <tibo.durand@gmail.com>
2019-07-10 15:55:25 +0200

    WIP: opendatacam in the wild section

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default () => (
           mb-16`
         }>
           <h1 className={
-           `text-2xl 
+            `text-2xl 
             sm:text-3xl 
             md:text-4xl 
             lg:text-5xl 
@@ -25,8 +25,8 @@ export default () => (
           }>
             An Open Source Camera to Collect Your Spatial Data
           </h1>
-          <a 
-            href="https://github.com/opendatacam/opendatacam" 
+          <a
+            href="https://github.com/opendatacam/opendatacam"
             className="btn btn-black inline-flex mt-10 flex items-center justify-center"
           >
             Installation guide
@@ -87,7 +87,104 @@ export default () => (
       <h2 className="section-title">App demo</h2>
       <div className="rounded-lg border border-solid border-gray-400 p-4 sm:p-8 sm:pl-12 sm:pr-12 m-4">
         <div className="video-responsive">
-          <iframe src="https://player.vimeo.com/video/346340651" width="640" height="332" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+          <iframe src="https://player.vimeo.com/video/346340651" width="640" height="332" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen></iframe>
+        </div>
+      </div>
+    </div>
+    <div className="container mx-auto" id="features">
+      <h2 className="section-title">Features</h2>
+      <div className="section-content section-content--reverse">
+        <div className="section-content-box">
+          <div className="section-content-title">
+            You own the data!
+          </div>
+          <div className="section-content-text">
+            OpenDataCam is not connected to the cloud, you’re in full control of when to gather it. Via the interface you can specify which areas of the picture objects should be counted. Also the camera tracks how things move through the frames. Via an easy export function you can access the tally and traces and use it in any thinkable way. Going all-in? Use the API to get access to the raw data points.
+          </div>
+        </div>
+        <div className="section-content-box">
+          <img className="section-content-img" src="static/images/about-1.jpg" alt="Camera" />
+        </div>
+      </div>
+    </div>
+    <div className="container mx-auto" id="ethics">
+      <h2 className="section-title">Ethics</h2>
+      <div className="section-content">
+        <div className="section-content-box">
+          <div className="section-content-title">
+            Transparency
+          </div>
+          <div className="section-content-text">
+            The discussion around the use of artificial intelligence in public space is ongoing. We believe OpenDataCam is a contribution to this discussion, as it offers an offline, transparent, thus privacy respecting alternative. Paramount to the open source access is the responsible use of this technology we request from every user.
+          </div>
+        </div>
+        <div className="section-content-box">
+          <img className="section-content-img" src="static/images/about-1.jpg" alt="Camera" />
+        </div>
+      </div>
+      <div className="section-content section-content--reverse">
+        <div className="section-content-box">
+          <div className="section-content-title">
+            Privacy
+          </div>
+          <div className="section-content-text">
+            As OpenDataCam is an experimental prototype with public facing applications, privacy is something we tried to bake into the design of the tool. After getting in touch with Sensor Labs privacy label, we’re currently finding ways to apply the label to OpenDataCam.
+          </div>
+        </div>
+        <div className="section-content-box">
+          <img className="section-content-img" src="static/images/about-1.jpg" alt="Camera" />
+        </div>
+      </div>
+    </div>
+    <div className="container mx-auto" id="features">
+      <h2 className="section-title">OpenDataCam in the Wild</h2>
+      <div className="section-content">
+        <div className="section-content-box">
+          <div className="section-content-text">
+            After publishing the alpha version, we asked people what they did with it. These selected cases are here to show you what you can do with OpenDataCam.
+          </div>
+        </div>
+        <div className="section-content-box">
+          <div className="section-content-text">
+            Let us know about how you use OpenDataCam. Apart from that we also think OpenDatacam would be great to…Profiling in spaces like shops, train, stations, drones...
+          </div>
+        </div>
+      </div>
+      <div className="px-2 mt-12">
+        <div className="flex -mx-2">
+          <div className="w-1/3 px-8">
+            <a href="https://beatthetraffic.moovellab.com">
+              <div className="card">
+                <div className="card-bordertop" />
+                <div className="card-title">Research Project with HTW & Technologiestiftung</div>
+                <div className="card-text mt-4">In coordination with the Berlin traffic administration and advising planners, we’d like to explore how the OpenDataCam performs in real-world settings. We’ll take a closer look on accuracy and test scenarios and data evaluation.</div>
+                <img className="mt-8" src="static/icons/arrow-right-blue.svg" />
+                <img className="w-full mt-4" src="static/images/street-jetsonnano.jpg" />
+              </div>
+            </a>
+          </div>
+          <div className="w-1/3 px-8">
+            <a href="https://beatthetraffic.moovellab.com">
+              <div className="card">
+                <div className="card-bordertop" />
+                <div className="card-title">Logistics NRW Dashboard</div>
+                <div className="card-text mt-4">The “Crowd Solving” project has implemented a Logistics Dashboard including various webcam locations where OpenDataCam is supposed to be implemented to create traffic data around the Neuss Harbour area. </div>
+                <img className="mt-8" src="static/icons/arrow-right-green.svg" />
+                <img className="w-full mt-4" src="static/images/street-jetsonnano.jpg" />
+              </div>
+            </a>
+          </div>
+          <div className="w-1/3 px-8">
+            <a href="https://beatthetraffic.moovellab.com">
+              <div className="card">
+                <div className="card-bordertop" />
+                <div className="card-title">Beat the traffic</div>
+                <div className="card-text mt-4">In coordination with the Berlin traffic administration and advising planners, we’d like to explore how the OpenDataCam performs in real-world settings. We’ll take a closer look on accuracy and test scenarios and data evaluation.</div>
+                <img className="mt-8" src="static/icons/arrow-right-yellow.svg" />
+                <img className="w-full mt-4" src="static/images/street-jetsonnano.jpg" />
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </div>
