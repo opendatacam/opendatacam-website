5e977b2ece59e3069cdefb441af758088c0db356
Florian Porada <florian.porada@gmail.com>
2019-07-16 11:44:32 +0200

    Fix absolute path issues in next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-const withCSS = require('@zeit/next-css')
+const withCSS = require('@zeit/next-css');
+
+const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = withCSS({
-    publicRuntimeConfig: {
-        URL_PREFIX: process.env.URL_PREFIX || "",
-        ROOT_URL: process.env.ROOT_URL || ""
-    }
-})
+  publicRuntimeConfig: {
+    URL_PREFIX: process.env.URL_PREFIX || '',
+    ROOT_URL: process.env.ROOT_URL || ''
+  },
+  assetPrefix: isProd ? './' : ''
+});
