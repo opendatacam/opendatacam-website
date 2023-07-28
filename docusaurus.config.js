a0ad14cfd04f7ca256853e5578c0ac095276c6a6
vsaw <hello@vsaw.net>
2023-07-28 09:40:35 +0200

    Create Sitemap for better SEO indexing and Ranking Fixes #72

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,6 +50,12 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/opendatacam/', '/docs/**'],
+          filename: 'sitemap.xml',
+        },
       }),
     ],
   ],
